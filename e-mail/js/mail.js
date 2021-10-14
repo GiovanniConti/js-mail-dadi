242395ac3fc07f9b3a95d63b891ada3ef457a0d6
GiovanniConti <giovanni.conti@live.it>
2021-10-14 09:46:21 +0200

    fix to "toLowerCase"

--- e-mail/js/mail.js
+++ e-mail/js/mail.js
@@ -2,16 +2,16 @@ const emailArray = ["a@b.com", "a@b.it", "a@b.co.uk"];
 const email = prompt("Inserisci la tua e-mail.");
 let validator = false;
 
-console.log(email)
+
 
 for (let i = 0; i < emailArray.length && validator  != true ; i++) {
     
-    if(emailArray[i] === email.toLowerCase){
+    if(emailArray[i] === email.toLowerCase()){
         validator = true;
     } else{
         validator = false;
     }
-    
+    console.log(email)
 }
 
 if (validator === true){
