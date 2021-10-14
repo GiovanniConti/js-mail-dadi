0eed029f5593bc29c953608a6cb290683a1cfab0
GiovanniConti <giovanni.conti@live.it>
2021-10-14 09:38:17 +0200

    changed validator with a boolean value

--- e-mail/js/mail.js
+++ e-mail/js/mail.js
@@ -1,20 +1,20 @@
 const emailArray = ["a@b.com", "a@b.it", "a@b.co.uk"];
 const email = prompt("Inserisci la tua e-mail.");
-let x = 0;
+let validator = false;
 
 console.log(email)
 
-for (let i = 0; i < emailArray.length && x < 1; i++) {
+for (let i = 0; i < emailArray.length && validator  != true ; i++) {
     
     if(emailArray[i] === email){
-        x++;
+        validator = true;
     } else{
-        x=x;   
+        validator = false;
     }
     
 }
 
-if (x == 1){
+if (validator === true){
     alert("ACCESS GRANTED");
 } else{
     alert("ACCESS DENIED");
